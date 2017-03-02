11718bae668e7e2637706f0a14ab8a5af95eae99
theborakompanioni <theborakompanioni+github@gmail.com>
2017-03-02 02:06:58 +0100

    add prototype "event list" to dashboard view

--- src/main/ionic/www/js/controllers.js
+++ src/main/ionic/www/js/controllers.js
@@ -1,6 +1,21 @@
 angular.module('starter.controllers', [])
 
-  .controller('DashCtrl', function ($scope) {
+  .controller('DashCtrl', function ($scope, Goals) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    $scope.goals = [];
+    Goals.page(0, 10).then(function (page) {
+      $scope.goals = page._embedded.goal;
+    });
+
+    $scope.remove = function (goal) {
+      Goals.remove(goal);
+    };
   })
   .controller('ProfileCtrl', function ($scope, Users) {
 
